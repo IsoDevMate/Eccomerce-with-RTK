b8041d8fe357331370cd799d01363603008544b5
IsoDevMate <oumaoduor5827@gmail.com>
2023-08-16 10:26:59 +0300

    display product-detailss on the ui

--- frontend/shopifyke/src/components/home.jsx
+++ frontend/shopifyke/src/components/home.jsx
@@ -1,13 +1,36 @@
-
+/* import { UseSelector } from "react-redux/es/hooks/useSelector" */ //if you use create async thunk
 import { useGetProductsByNameQuery } from "../features/productAPIS"
 export const Home = () => {
-  const { data, error/* isFetching, isLoading */ }= useGetProductsByNameQuery('')
+/*   const { products,status } = UseSelector((state) => state.product) */
+  const { data, error,/* isFetching,*/ isLoading  }= useGetProductsByNameQuery()
   console.log(data)
   console.log(error)
 
 
   return (
-    <div>home</div>
+    
+    <div className="container">
+      {isLoading ? (<h1>Loading...</h1> ): error ? (<h1>An Error Occurred.</h1>) : 
+      <>
+      <h3>NEW Products</h3>
+      <div className="products"></div>
+      {data?.map(product =>(<div key={product.id} className="product">
+        
+        <h3>{product.name}</h3>
+       {  <img src={product.Image} alt={product.name} /> }
+       <div className="details">
+         <span>{product.description}</span>
+         <span className="price">{product.price}</span>
+       </div>
+          <button>ADD TO CART{product.price}</button>
+        </div>)      
+      )}
+        </>
+     }
+    </div>
+    
+    
+    
   )
 }
 
