0403b2534d6920818a681b07f234665124c0e642
IsoDevMate <oumaoduor5827@gmail.com>
2023-09-15 19:10:55 +0300

    intergrate jwt

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Joi =require('joi')
+const { User }=require('../models/schema')
 
 exports.Validater=asyncHandler(async (req, res,next) => {
     const schema=Joi.object({
@@ -14,3 +15,35 @@ exports.Validater=asyncHandler(async (req, res,next) => {
     }
     next()
 })
+//lets bring in the user model that aids us interact with the db
+let user = await User.findOne({ email:req.body.email })
+if(user) return res.status(400).send('User already exists...')
+
+//generate a document to be saved to the Db
+user = new User({
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password,
+})
+//encrypt the password
+
+const saltRounds = 10
+
+const salt= await bcrypt.genSalt(saltRounds)
+user.password = await bcrypt.hash(user.password,salt)
+
+//comparing db password 
+
+/* const checkUser=(async(username, password) =>{
+    //... fetch user from a db etc.
+
+    const match = await bcrypt.compare(user.password, user.passwordHash);
+
+    if(match) {
+        //login
+    }})
+//lets save the user to the db
+checkUser()
+ */
+await user.save()
+
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,4 +1,4 @@
-const rouhter = require('express').Router();
+const router = require('express').Router();
 const { Validater } = require('../controllers/controller');
 
 router.post('/', Validater);
